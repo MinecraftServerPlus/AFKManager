6064b612ee4586dacf97d92dd08e851848045d5f
9um4 <sweetenpotato@outlook.com>
2023-08-06 22:44:14 +0900

    init: Initialize Project

---
+++ src/main/java/io/github/gum4/afkmanager/AFKManager.java
@@ -0,0 +1,17 @@
+package io.github.gum4.afkmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AFKManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
